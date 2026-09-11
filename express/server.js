e13f6dc0d369873ddc5616ac7853ec52c459f501
Gabbar-Sharma <gabbarsharma75663@gmail.com>
2026-09-11 07:31:14 +0530

    adding post api

--- express/server.js
+++ express/server.js
@@ -1,1 +1,21 @@
-console.log("hello bro")
+const express = require('express');
+const app = express()
+const port = 3000
+app.use(express.json())
+const products = []
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.post("/product", (req, res) =>{
+     console.log("REQUEST BODY:", req.body);
+        res.send(req.body);
+        products.push(req.body)
+
+        res.status(201).json
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

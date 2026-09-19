6ae98dbbfbf895b2455e6bb56d134f8397c91c66
Gabbar-Sharma <gabbarsharma75663@gmail.com>
2026-09-20 00:04:25 +0530

    feat: add dotenv for environment variable management and configure MongoDB connection

--- file-handling/src/app.js
+++ file-handling/src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const route = require("./router/file.route")
 
 const app = express()
-
+app.use("/api/file", route)
 app.get('/', (req, res) =>{
     res.send("Api created succussfully");
 })
---
+++ file-handling/src/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+
+
+ const connectDb = async() =>{
+      try{
+           await mongoose.connect(process.env.MONGO_URI)
+           console.log("MongoDB connected successfully");
+      }
+      catch(error){
+        console.log("server mai db error de rha h", error.message)
+         process.exit(1)
+      }
+ }
+
+
+ module.exports = connectDb;

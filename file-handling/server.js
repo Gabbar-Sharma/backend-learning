856967d4e63b06c7823492f9f50c744f1c25764e
Gabbar-Sharma <gabbarsharma75663@gmail.com>
2026-09-20 18:34:24 +0530

    feat: update MongoDB URI in .env and establish database connection in server.js

--- file-handling/server.js
+++ file-handling/server.js
@@ -1,7 +1,9 @@
 const app = require('./src/app')
-
+const connectDb = require("./src/config/db")
+require("dotenv").config()
 const port = 4000;
 
+connectDb()
 
 app.listen(port, () =>{
     console.log(`server running on ${port}`)

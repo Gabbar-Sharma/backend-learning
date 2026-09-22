ce791143172a447b7159ebd3b6553faf6b27f9c7
Gabbar-Sharma <gabbarsharma75663@gmail.com>
2026-09-22 22:58:24 +0530

    adding new project authentication

---
+++ authentication/backend/src/app/app.js
@@ -0,0 +1,44 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+const app = express();
+app.use(express.json());
+app.get("/api", (req, res) => {
+  try {
+    return res.status(200).json({
+      message: "get api created successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "something went wrong brother",
+    });
+  }
+});
+
+app.post("/api/register", (req, res) => {
+  const { email, name, password } = req.body;
+// save data on mongodb
+// token create here
+const token = jwt.sign(
+  {
+    email,
+    name,
+  },
+  // jwt secreat
+  "frKTcYPV8y6wQkxi0AHYxnljCx5ELcCXuffhr1Y9b13",
+  res.status(201).json({
+    message: "user created successfully",
+    user: {
+      email,
+      password,
+    },
+    token,
+  }),
+);
+
+
+});
+
+
+
+
+export default app;
---
+++ authentication/backend/src/server.js
@@ -0,0 +1,9 @@
+import app from "./app/app.js";
+
+
+const port = 3000
+
+app.listen(port, () =>{
+    console.log("server running on port 3000")
+})
+

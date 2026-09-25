43e318ec8aab7e52b78c9b78196fdcbcefed1605
Gabbar-Sharma <gabbarsharma75663@gmail.com>
2026-09-25 13:21:31 +0530

    Refactor authenticate middleware to handle async operations and improve error responses

--- integrate-auth/src/app/middleware/auth.middleware.js
+++ integrate-auth/src/app/middleware/auth.middleware.js
@@ -1,12 +1,48 @@
 import jwt from "jsonwebtoken"
 import User from "../models/user.model.js"
 
-const authenticate = (req, res, next) =>{
+const authenticate = async(req, res, next) =>{
     try{
      //get Authorization header
-     const authHeader = 
-    }
-    catch(error){
+     const authHeader = req.header.authorization;
+     if(!authHeader){
+        return res.status(401).json({
+            message: "Authorization header missing"
+        })
+     }
+
+     // Bearer token extract
+     const [scheme, token] = authHeader.split(" ")
+       
+     if(scheme !== "Bearer" || !token){
+        return res.status(401).json({
+            message: "Invalid authorization format"
+        })
+     }
+   
+       //Access token verify
+       const decoded = jwt.verify(
+        token,
+        process.env.JWT_SECRET
+       )
+ // Token se User ID lekar Database mai user find karna
+     const user = await User.findById(decoded.id)
+     if(!user){
+        return res.status(401).json({
+             message: "User not found",
+        })
+     }
+
+     // user ko req ke ander attach 
+     req.user = user
+     //next conttroller/middlewere 
+     next();
 
     }
+    catch(error){
+ return res.status(401).json({
+      message: "Invalid or expired access token",
+    });
+    }
 }
+export default authenticate;
